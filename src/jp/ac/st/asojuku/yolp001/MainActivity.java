33052e90c146357e82371fc41df6f43f43a9eeba
aso1201728 <1201728@st.asojuku.ac.jp>
2014-07-16 15:17:03 +0900

    2017/07/16 3rd

--- src/jp/ac/st/asojuku/yolp001/MainActivity.java
+++ src/jp/ac/st/asojuku/yolp001/MainActivity.java
@@ -67,18 +67,16 @@ public class MainActivity extends Activity implements LocationListener, WeatherO
 
 	}
 
-
-
 	@Override
 	public void errorUpdateWeather(WeatherOverlay arg0, int arg1) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
 
 	@Override
 	public void finishUpdateWeather(WeatherOverlay arg0) {
 		// TODO 自動生成されたメソッド・スタブ
-		
+
 	}
 
 	@Override
@@ -93,7 +91,7 @@ public class MainActivity extends Activity implements LocationListener, WeatherO
 		super.onResume();
 
 		// 地図表示用のYahooライブラリview部品を用意
-		mMapView = new MapView(this,"dj0zaiZpPTdhZ1hERlB4QU01ViZzPWNvbnN1bWVyc2VjcmV0Jng9Mjgdj0zaiZpPTdhZ1hERlB4QU01ViZzPWNvbnN1bWVyc2VjcmV0Jng9Mjg");
+		mMapView = new MapView(this,"dj0zaiZpPTdhZ1hERlB4QU01ViZzPWNvbnN1bWVyc2VjcmV0Jng9Mjg-");
 		// ズームボタンを画面にON
 		mMapView.setBuiltInZoomControls(true);
 		// 地図縮尺バーを画面にON
